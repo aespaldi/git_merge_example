7d9e75ffc361a66205fefb5559b785e497a9425f
Zee Spencer <zee@zeespencer.com>
2013-05-17 21:10:06 +0000

    Add dance move to index.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,10 @@
 get '/' do
   # Look in app/views/index.erb
+  @move = "super sonic dance attack!"
   erb :index
 end
 
 get '/session' do
-	@user="bob"
+	@user="User"
 	@color = "monkey_puke_green"
 end
